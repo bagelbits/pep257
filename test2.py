35debf5bc5e8c22b7ecfa8be5034b6f2d8e37acc
Jeremy Keeshin <jkeesh@gmail.com>
2013-06-30 20:30:33 -0700

    add test

---
+++ test2.py
@@ -0,0 +1,19 @@
+"""This file will pass the checks."""
+
+
+def main(x, y, z):
+    """Execute the program.
+
+    Parameters
+    ----------
+    x: type
+        description
+    y: type
+        description
+    z: type
+        description
+
+    """
+    print 5 + x
+
+main(3, 4, 5)
